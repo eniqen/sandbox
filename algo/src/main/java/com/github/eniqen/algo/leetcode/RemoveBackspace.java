499de58dbeee7c5de3264f780c2d64bb55877cef
m.nemenko <r2n@bk.ru>
2020-04-27 00:46:42 +0530

    remove backspace

---
+++ algo/src/main/java/com/github/eniqen/algo/leetcode/RemoveBackspace.java
@@ -0,0 +1,38 @@
+package com.github.eniqen.algo.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author Mikhail Nemenko {@literal <nemenkoma@gmail.com>}
+ */
+public class RemoveBackspace {
+	public static void main(String[] args) {
+	}
+
+    static public boolean backspaceCompare(String S, String T) {
+        String x = removeBackspace(S);
+        String y = removeBackspace(T);
+        System.out.println(x);
+        System.out.println(y);
+        return x.equals(y);
+    }
+    
+      static String removeBackspace(String s) {
+          int backCount = 0;
+          StringBuilder sb = new StringBuilder();
+          for(int i = s.length() - 1; i >= 0; i--) {
+              char ch = s.charAt(i);
+              if(ch =='#') {
+                  backCount++;
+                  continue;
+              } else {
+                  if(backCount < 1) {
+                      sb.append(ch);
+                  } else {
+                      backCount--;
+                  }
+              }
+          }
+      return sb.toString();
+          }
+}
