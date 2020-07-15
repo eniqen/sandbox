71487c65a6ce75d19612015572b7a6c4a87ef7fb
eniqen <r2n@bk.ru>
2020-07-15 23:51:54 +0300

    349. Intersection of Two Arrays - easy

---
+++ algo/src/main/java/com/github/eniqen/algo/leetcode/TwoArrayIntersection.java
@@ -0,0 +1,44 @@
+package com.github.eniqen.algo.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Mikhail Nemenko {@literal <nemenkoma@gmail.com>}
+ */
+public class TwoArrayIntersection {
+	public static void main(String[] args) {
+		int [] nums1 = new int[]{4,9,5};
+		int [] nums2 = new int[]{9,4,9,8,4};
+
+		System.out.println(intersection(nums1, nums2));
+	}
+
+	public static int[] intersection(int[] nums1, int[] nums2) {
+		Set<Integer> n1 = toSet(nums1);
+		Set<Integer> n2 = toSet(nums2);
+
+		n1.retainAll(n2);
+
+		int [] result = new int[n1.size()];
+
+		int index = 0;
+
+		for(int x: n1) {
+			result[index] = x;
+			index++;
+		}
+
+		return result;
+	}
+
+	private static Set<Integer> toSet(int[] arr) {
+		Set<Integer> set = new HashSet<>();
+
+		for (int anArr : arr) {
+			set.add(anArr);
+		}
+
+		return set;
+	}
+}
