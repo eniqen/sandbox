5f86ac2d5baef52b93d778dfee449a7aeffc8bfa
eniqen <r2n@bk.ru>
2020-09-03 23:08:32 +0300

    206. Reverse Linked List - easy

---
+++ algo/src/main/java/com/github/eniqen/algo/leetcode/ReverseSinglyList.java
@@ -0,0 +1,22 @@
+package com.github.eniqen.algo.leetcode;
+
+/**
+ * @author Mikhail Nemenko {@literal <nemenkoma@gmail.com>}
+ */
+public class ReverseSinglyList {
+	public static void main(String[] args) {
+		ListNode root = ListNode.of(1, ListNode.of(2, ListNode.of(3, ListNode.of(4, ListNode.of(5)))));
+		System.out.println(reverseList(root));
+	}
+
+	private static ListNode reverseList(ListNode head) {
+		ListNode result = null;
+		while(head != null) {
+			ListNode temp = head;
+			head = head.next;
+			temp.next = result;
+			result = temp;
+		}
+		return result;
+	}
+}
