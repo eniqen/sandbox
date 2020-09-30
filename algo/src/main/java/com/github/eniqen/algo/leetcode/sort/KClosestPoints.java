396e5e290e697afb47e2b7bb85cbd51c80186092
eniqen <r2n@bk.ru>
2020-09-30 12:27:16 +0300

    973. K Closest Points to Origin o(n log k) - middle

--- algo/src/main/java/com/github/eniqen/algo/leetcode/sort/KClosestPoints.java
+++ algo/src/main/java/com/github/eniqen/algo/leetcode/sort/KClosestPoints.java
@@ -2,11 +2,13 @@ package com.github.eniqen.algo.leetcode.sort;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.function.ToIntFunction;
 
 /**
  * @author Mikhail Nemenko {@literal <nemenkoma@gmail.com>}
  */
-public class KClosestPointa {
+public class KClosestPoints {
 	public static void main(String[] args) {
 		int[][] points = new int[][]{
 			new int[]{3, 3},
@@ -18,6 +20,7 @@ public class KClosestPointa {
 		System.out.println(Arrays.deepToString(kClosest(points, k)));
 	}
 
+	//O(n log N)
 	private static int[][] kClosest(int[][] points, int k) {
 
 		if(k <= 0) return new int[0][0];
@@ -28,6 +31,26 @@ public class KClosestPointa {
 		int[][] result = new int[k][2];
 
 		System.arraycopy(points, 0, result, 0, k);
+
+		return result;
+	}
+
+	private static int[][] realTimeKClosest (int[][] points, int k) {
+		PriorityQueue<int[]> q = new PriorityQueue<>(Comparator.comparingInt((ToIntFunction<int[]>) p -> p[0] * p[0] + p[1] * p[1]).reversed());
+
+		for(int[] p: points) {
+			q.offer(p);
+
+			if(q.size() > k) {
+				q.poll();
+			}
+		}
+
+		int[][] result = new int[k][2];
+
+		while(k > 0) {
+			result[--k] = q.poll();
+		}
 
 		return result;
 	}
