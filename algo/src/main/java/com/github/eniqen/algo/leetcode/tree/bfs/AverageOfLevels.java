76bb814bdd2f845d1e9914656f8deb8f0bf9726f
eniqen <r2n@bk.ru>
2020-10-19 19:57:08 +0300

    637. Average of Levels in Binary Tree - recursive version

--- algo/src/main/java/com/github/eniqen/algo/leetcode/tree/bfs/AverageOfLevels.java
+++ algo/src/main/java/com/github/eniqen/algo/leetcode/tree/bfs/AverageOfLevels.java
@@ -14,6 +14,7 @@ public class AverageOfLevels {
 	public static void main(String[] args) {
 		TreeNode root = TreeNode.of(3, TreeNode.of(9), TreeNode.of(20, TreeNode.of(15), TreeNode.of(7)));
 		System.out.println(averageOfLevels(root));
+		System.out.println(avg(root));
 	}
 
 	private static List<Double> averageOfLevels(TreeNode root) {
@@ -34,5 +35,33 @@ public class AverageOfLevels {
 		}
 
 		return result;
+	}
+
+	private static List<Double> avg(TreeNode root) {
+		if(root == null) return new ArrayList<>();
+		List<Integer> sum = new ArrayList<>();
+		List<Integer> counts = new ArrayList<>();
+		avgRec(root, sum, counts, 0);
+		List<Double> result = new ArrayList<>();
+
+		for(int i = 0; i < counts.size(); i++) {
+			result.add(1.0 * sum.get(i) / counts.get(i));
+		}
+		return result;
+	}
+
+	private static void avgRec(TreeNode t, List<Integer> s, List<Integer> c, int level) {
+		if(t == null) return;
+
+		if(level < s.size()) {
+			s.set(level, s.get(level) + t.val);
+			c.set(level, c.get(level) + 1);
+		} else {
+			s.add(t.val);
+			c.add(1);
+		}
+
+		avgRec(t.left, s, c, level + 1);
+		avgRec(t.right, s, c, level + 1);
 	}
 }
